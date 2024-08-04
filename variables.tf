ac5a26ad94faade1e9203496401be1f409e17493
raj470 <71519393+raj470@users.noreply.github.com>
2024-08-04 15:31:21 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "region" {
+   description = "AWS Region"
+   default = "us-east-1"
+}
+variable "instance_type" {
+  description = "Instance Type"
+  default = "t2.micro"
+}
+variable "instance_name" {
+  description = "EC2 Instance Name"
+  default = "Provisioned by Terraform"
+}
+variable "ami" {
+  description = "AMI for instance"
+  default = "ami-0a1179631ec8933d7"
+}
+variable "tags" {
+  type = map(string)
+  description = "Tagging the resource"
+  default = {
+    "Enivornment" = "prod"
+    "Provisioned" = "terraform"
+  }
+}
+variable "key_name" {
+  description = "The key name of instance"
+  default = "test_ec2"
+}
+variable "vpc_security_group_ids" {
+  description = "VPC Security Groups"
+  default = ["sg-096e76d2c6315fbaf"]
+}
+variable "availability_zone" {
+  description = "Defining availability_zone"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+}
